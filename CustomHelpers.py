ebab9a7d881ca91a9225946c6f8b07dfcf1b9f7c
functorial <alexpokorny92@gmail.com>
2021-02-14 03:40:56 -0800

    Add to CustomPipelines.py and finish conclusions.

--- CustomHelpers.py
+++ CustomHelpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_selection import chi2, f_classif
+from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
@@ -18,9 +19,13 @@ def get_complement(sup:pd.DataFrame, sub:pd.DataFrame) -> pd.DataFrame:
     return complement
 
 
-def stacked_hist(feature:str, df, target):
-    plt.hist(x=[df.loc[train_df[target] == True,  feature],
-                df.loc[train_df[target] == False, feature]],
+def stacked_hist(feature:str, df:pd.DataFrame, target:str):
+    """
+    Returns a stacked histogram where the feature values in the supplied DataFrame
+    are grouped by color based on their target values. 
+    """
+    plt.hist(x=[df.loc[df[target] == True,  feature],
+                df.loc[df[target] == False, feature]],
              bins=200, stacked=True, color=['tab:orange','tab:blue'])
     plt.xlabel(feature)
     
@@ -61,7 +66,12 @@ def get_anova(X, y):
     return stats
 
 
-def univariate_summary(feature:str, df, target, chi2_stats:dict, anova_stats:dict, alpha:float=0.05):
+def univariate_summary(feature:str, df:pd.DataFrame, target:str, 
+                       chi2_stats:dict, anova_stats:dict, alpha:float=0.05):
+    """
+    Returns some exploratory information about a single column in the supplied DataFrame.
+    Use `get_chi2` and `get_anova` to supply the `chi2_stats` and `anova_stats` arguments.
+    """
     stacked_hist(feature, df=df, target='Attrition_Flag')
 
     try:
@@ -83,3 +93,97 @@ def univariate_summary(feature:str, df, target, chi2_stats:dict, anova_stats:dic
 
     print(f'ANOVA F-Test p-value: {anova_p:.3f}')
     print(f'ANOVA: {feature} is probably independent of target: {anova_is_ind}')
+
+
+def get_sorted_correlations(df:pd.DataFrame) -> pd.DataFrame:
+    """
+    Inputs a pandas DataFrame and returns a Pandas DataFrame with rows indexed 
+    by pairs (feature1, feature2). The absolute value of the correlation between
+    each pair is returned as a column and the rows are sorted by this column in 
+    descending order.
+    """
+    features1 = list(df.corr().columns.copy())
+    features2 = list(df.corr().index.copy())
+    n = len(features1)
+    num_rows = int(n*(n-1)/2)
+    rows = [i for i in range(num_rows)]
+
+    corrs_df = df.corr()
+    pairs = []
+    for f in features1:
+        features2.remove(f)
+        for g in features2:
+            pairs.append( ((f, g), corrs_df.loc[f, g]) )
+
+    corrs_sorted = pd.DataFrame(index=rows, columns=('Features', 'Abs_Correlation'))
+    for row in rows:
+        pair = pairs.pop(0)
+        corrs_sorted.loc[row, 'Features'] = pair[0]
+        corrs_sorted.loc[row, 'Abs_Correlation'] = pair[1]
+
+    corrs_sorted['Is_Positive_Correlation'] = corrs_sorted['Abs_Correlation'] > 0
+    corrs_sorted['Abs_Correlation'] = corrs_sorted['Abs_Correlation'].abs()
+    corrs_sorted.sort_values(by='Abs_Correlation', ascending=False, inplace=True)
+    corrs_sorted.reset_index(drop=True, inplace=True)    
+
+    return corrs_sorted 
+
+
+def stacked_scatterplot(df:pd.DataFrame, feature1:str, feature2:str, target:str):
+    """
+    Returns a stacked scatterplot where the feature values in the supplied DataFrame
+    are grouped by color based on their target values. 
+    """
+    p0 = df.loc[df[target] == 0, [feature1, feature2]]
+    p1 = df.loc[df[target] == 1, [feature1, feature2]]
+
+    x0 = p0[feature1]
+    y0 = p0[feature2]
+
+    x1 = p1[feature1]
+    y1 = p1[feature2]
+
+    plt.scatter(x0, y0, c='tab:blue', label=f'{target}: False')
+    plt.scatter(x1, y1, c='tab:orange', label=f'{target}: True')
+
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.legend()
+    plt.show()
+
+
+def compare_pr_curves(X, y, models:dict):
+    """
+    Plots precision vs recall for the supplied models, given as a dict of name, model pairs.
+    """
+
+    for model_name in models.keys():
+        model = models[model_name]
+        try:
+            y_scores = model.predict_proba(X)[:,1]
+            precisions, recalls, _ = precision_recall_curve(y, y_scores)
+            plt.plot(recalls, precisions, label=model_name)
+        except:
+            print(f'Warning: Could not get scores from {model_name}.\nModel may not be fit.')
+    
+    plt.grid()
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('PR Curve Comparison')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+def show_precision_recall_vs_threshold(X, y, model, title='Precision/Recall vs Thresholds'):
+    y_scores = model.predict_proba(X)[:,1]
+    precisions, recalls, thresholds = precision_recall_curve(y, y_scores)
+    plt.plot(thresholds, precisions[:-1], label='Precision')
+    plt.plot(thresholds, recalls[:-1], label='Recall')
+    plt.xlabel('Threshold')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def predict_with_threshold(X, model, threshold:float):
+    y_hat = model.predict_proba(X)[:,1]
+    return y_hat >= threshold
--- CustomPipelines.py
+++ CustomPipelines.py
@@ -17,20 +17,24 @@ class DropFeatures(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y=None):
-        X_copy = X.copy()
+        X_ = X.copy()
         if self.feature_list is not None:
             for feature in self.feature_list:
                 try:
-                    X_copy.drop(columns=feature, inplace=True)
+                    X_.drop(columns=feature, inplace=True)
                 except KeyError:
                     print(f'DropFeatures.transform Warning: {feature} does not exist.')
-        return X_copy
+
+        self.feature_list_ = X_.columns
+
+        return X_
 
 
 class CreateFeatures(BaseEstimator, TransformerMixin):
     """
     A transformer which augments new columns to a pandas dataframe. The new columns are 
-    given as a dictionary with keys the column names and the values as pandas series objects.
+    given as a dictionary with keys the column names and the values as functions which 
+    return pandas series objects.
     """
     def __init__(self, feature_dict):
         self.feature_dict = feature_dict
@@ -39,11 +43,14 @@ class CreateFeatures(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        X_copy = X.copy()
+        X_ = X.copy()
         if self.feature_dict.keys() is not None:
             for feature in self.feature_dict.keys():
-                X_copy[feature] = self.feature_dict[feature]
-        return X_copy
+                X_[feature] = self.feature_dict[feature](X_)
+
+        self.feature_list_ = X_.columns
+
+        return X_
 
 
 class TransformFeatures(BaseEstimator, TransformerMixin):
@@ -58,15 +65,122 @@ class TransformFeatures(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        X_copy = X.copy()
+        X_ = X.copy()
         if self.function_dict.keys() is not None:
             for feature in self.function_dict.keys():
                 try:
-                    X_copy[feature] = X_copy[feature].apply(self.function_dict[feature])
+                    X_[feature] = X_[feature].apply(self.function_dict[feature])
                 except KeyError:
                     print(f'TransformFeatures.transform Warning: {feature} does not exist.')
-        return X_copy
 
+        self.feature_list_ = X_.columns
+
+        return X_
+
+
+class CustomOneHot(BaseEstimator, TransformerMixin):
+    """
+    A custom one-hot encoder class. The main purpose of using this class over the encoder
+    provided by sklearn is that the custom encoder returns a pandas DataFrame. This way,
+    we can easily track the column names.
+
+    Drops one of the value columns, as well as the original column, 
+    in order to prevent colinearity of columns.
+    """
+    def __init__(self, columns:list):
+        self.columns = columns
+
+    def fit(self, X:pd.DataFrame, y=None):
+        return self
+
+    def transform(self, X:pd.DataFrame, y=None):
+        X_ = X.copy()
+        d = {False:0, True:1}
+
+        for column in self.columns:
+            try:
+                values = list(X_[column].unique())
+
+                for value in values[1:]:
+                    X_[f'{column}_is_{value}'] = X_[column] == value
+                    X_[f'{column}_is_{value}'].replace(d, inplace=True)
+                
+                X_.drop(columns=[column], inplace=True)
+            except KeyError:
+                print(f'CustomOneHot.transform Warning: {column} does not exist.')
+
+        self.feature_list_ = X_.columns
+
+        return X_
+
+
+class CustomOrdinal(BaseEstimator, TransformerMixin):
+    """
+    A custom one-hot encoder class. The main purpose of using this class over the encoder
+    provided by sklearn is that the custom encoder returns a pandas DataFrame. This way,
+    we can easily track the column names.
+
+    Drops the supplied columns after encoding.
+
+    The constructor takes as input a list of tuples `(column, ordering)`
+    where `column` is the name of the column and `ordering` is the tuple
+    of the values in their specified order.
+    """
+
+    def __init__(self, columns_orderings:tuple):
+        self.columns_orderings= columns_orderings
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+
+        for pair in self.columns_orderings:
+            column = pair[0]
+            ordering = pair[1]
+            d = {}
+
+            for i in range(len(ordering)):
+                value = ordering[i]
+                d[value] = i
+            
+            try:
+                X_[column].replace(d, inplace=True)
+            except KeyError:
+                print(f'CustomOrdinal.transform Warning: {column} does not exist.')
+
+        self.feature_list_ = X_.columns
+
+        return X_
+
+
+class CustomScaler(BaseEstimator, TransformerMixin):
+    """
+    A transformer similar to `StandardScaler` from sklearn, except it will return
+    a pandas DataFrame instead. 
+    """
+
+    def __init__(self):
+        return None
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_ = X.copy()
+
+        for column in X_.columns:
+            mean = X_[column].mean()
+            std  = X_[column].std()
+
+            X_[column] = (X_[column] - mean) / std
+
+        self.feature_list_ = X_.columns
+        return X_
+
+
+# TODO: Debug or rewrite this.
 class RFCImputer(BaseEstimator, TransformerMixin):
     """
     A custom random forest classifier imputer. Load the constructor with an imputer model. Fit will train the imputer model.
@@ -119,7 +233,7 @@ class RFCImputer(BaseEstimator, TransformerMixin):
         X_ = X.copy()
 
         for feature in self.features:
-            X_missing_feature = X_.loc[X_[feature] == self.missing_value].drop(columns=features)
+            X_missing_feature = X_.loc[X_[feature] == self.missing_value].drop(columns=self.features)
             y_hat_feature = self.feature_model_dict[feature].predict(X_missing_feature)
 
             X_.loc[X_.feature == self.missing_value] = y_hat_feature
